a375ebfe848cff0376be5320af430f8f34498db5
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-21 18:01:51 +0300

    Update hw1_invertint.py

--- homework1/hw1_invertint.py
+++ homework1/hw1_invertint.py
@@ -1,10 +1,16 @@
 def reverse(number):
     n2 = 0
-    n1 = number
+    if number < 0:
+        n1 = -number
+    else:
+        n1 = number
     while n1 > 0:
         digit = n1 % 10;  # находим остаток - последнюю цифру числа
         n1 = n1 // 10;  # делим нацело - убираем из числа последнюю цифру
         n2 = n2 * 10  # увеличиваем разрядность второго числа
         n2 = n2 + digit  # добавляем очередную цифру
-    return n2
+    if number < 0:
+        return -n2
+    else:
+        return n2
     raise NotImplementedError
