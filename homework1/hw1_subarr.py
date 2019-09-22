a8c6e66333452c42b775aa43dca543180bdd52fe
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-22 22:43:40 +0300

    Create hw1_subarr.py

---
+++ homework1/hw1_subarr.py
@@ -0,0 +1,12 @@
+def find_subarr(input_lst, num):
+    dict = {}
+    sum = 0
+    for idx, val in enumerate(input_lst):
+        sum +=val
+        if sum - num in dict:
+            return (dict[sum-num],idx)
+        elif val == num:
+            return (idx,idx)
+        else:
+            dict[sum - val] = idx
+    return ()
